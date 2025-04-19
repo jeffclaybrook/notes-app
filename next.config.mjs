1f46f968ab2d0ce1bdbad7837eeb4a64d33593f2
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-04-19 15:06:17 -0500

    Created PWA

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import withPWA from "next-pwa"
+
+const isDev = process.env.NODE_ENV === "development"
+
+const nextConfig = {
+ reactStrictMode: true,
+ swcMinify: true,
+ pwa: {
+  dest: "public",
+  disable: isDev,
+  register: true,
+  skipWaiting: true,
+  fallbacks: {
+   document: "/offline.html"
+  }
+ }
+}
+
+export default withPWA(nextConfig)

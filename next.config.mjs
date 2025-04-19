4a23b7d276061611bb1b988e9dc3018423e3e847
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-04-19 15:09:24 -0500

    Updated next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,17 @@ const isDev = process.env.NODE_ENV === "development"
 
 const nextConfig = {
  reactStrictMode: true,
- swcMinify: true,
- pwa: {
-  dest: "public",
-  disable: isDev,
-  register: true,
-  skipWaiting: true,
-  fallbacks: {
-   document: "/offline.html"
-  }
+ swcMinify: true
+}
+
+const pwaConfig = {
+ dest: "public",
+ disable: isDev,
+ register: true,
+ skipWaiting: true,
+ fallbacks: {
+  document: "/offline.html"
  }
 }
 
-export default withPWA(nextConfig)
+export default withPWA(pwaConfig)(nextConfig)

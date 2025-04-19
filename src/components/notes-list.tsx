86c58d1f242402788a2cd1525a57d84733e80347
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-04-19 15:20:18 -0500

    Updated notes list gap and padding on mobile

--- src/components/notes-list.tsx
+++ src/components/notes-list.tsx
@@ -11,7 +11,7 @@ type NotesListProps = {
 
 export default function NotesList({ notes, view, onNoteClick }: NotesListProps) {
  return (
-  <section className={view === "grid" ? "grid grid-cols-2 lg:grid-cols-4 gap-4 p-4" : "flex flex-col gap-4 p-4 max-w-3xl mx-auto"}>
+  <section className={view === "grid" ? "grid grid-cols-2 lg:grid-cols-4 gap-2 p-2 lg:gap-4 lg:p-4" : "flex flex-col gap-2 p-2 lg:gap-4 lg:p-4 max-w-3xl mx-auto"}>
    {notes.map(note => (
     <NoteCard
      key={note._id}

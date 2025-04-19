61c8b8a44d54fb65f6219cc85df1ef4c8a822d70
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-04-19 14:33:31 -0500

    Updated [id]/route.ts again

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -2,23 +2,19 @@ import { NextResponse } from "next/server"
 import { db } from "@/lib/db"
 import Note from "@/models/note"
 
-export async function PATCH(
- req: Request,
- { params }: { params: { id: string } }
-) {
+export async function PATCH(req: Request) {
  await db()
- const { id } = params
+ const url = new URL(req.url)
+ const id = url.pathname.split("/").pop()
  const data = await req.json()
  const note = await Note.findByIdAndUpdate(id, data, { new: true })
  return NextResponse.json(note)
 }
 
-export async function DELETE(
- req: Request,
- { params }: { params: { id: string } }
-) {
+export async function DELETE(req: Request) {
  await db()
- const { id } = params
+ const url = new URL(req.url)
+ const id = url.pathname.split("/").pop()
  await Note.findByIdAndDelete(id)
  return NextResponse.json({ success: true })
 }
